5f165e6304ab7c772d79e46f4bf63d9d50fe58d6
alejandrosalgar <alejandro.salgar@gmail.com>
2021-09-19 19:33:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mbgsey
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mbgsey)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Metrologia y diseño</h1>
+      <p>Hola</p>
+    </div>
+  );
+}
